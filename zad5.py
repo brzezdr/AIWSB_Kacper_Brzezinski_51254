ac6885a1b142262dc757f63cee91b43e8666690d
brzezdr <97391732+brzezdr@users.noreply.github.com>
2022-02-26 16:23:16 +0100

    Add files via upload

---
+++ zad5.py
@@ -0,0 +1,4 @@
+a = 2
+b = 3
+print ('Liczba a = ', a, ", Liczba b =", b)
+print ("Suma kwadratow tych liczb to ", a*a + b*b)
---
+++ zad6.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+a = 4;
+h = 2;
+print ("pole trojkata = ", a*h/2);
